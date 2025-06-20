33c5c1933f63b00d3cbadac873d142f8560cbd6d
Beso Genebashvili <genebashvili99@gmail.com>
2025-06-20 18:11:44 +0400

    Refactored code, separated AnsiConsole logic

---
+++ GameOfLife/GameOfLife/AnsiConsoleExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Spectre.Console;
+
+namespace GameOfLife;
+
+public sealed class AnsiConsoleExtensions
+{
+    public static Pattern.Name PromptPattern() =>
+        AnsiConsole.Prompt(
+            new SelectionPrompt<Pattern.Name>()
+                .Title("Select [green]Pattern[/]:")
+                .AddChoices(Enum.GetValues<Pattern.Name>()));
+
+    public static void MarkupLineProperties(
+        int speed,
+        Pattern.Name patternName,
+        long generation)
+    {
+        AnsiConsole.MarkupLine($"• Speed: [green]{speed}ms[/]");
+        AnsiConsole.MarkupLine($"• Pattern: [green]{patternName}[/]");
+        AnsiConsole.MarkupLine($"• Generation: [green]{generation}[/]");
+    }
+
+    public static void DrawGenerationWithCanvas(int[][] xs)
+    {
+        var canvas = new Canvas(xs.Length, xs[0].Length);
+
+        for (int i = 0; i < xs.Length; i++)
+        {
+            for (int j = 0; j < xs[i].Length; j++)
+            {
+                var color = ResolveColor(xs[i][j]);
+
+                // j is the X coordinate (column)
+                // i is the Y coordinate (row)
+                canvas.SetPixel(j, i, color);
+            }
+        }
+
+        AnsiConsole.Write(canvas);
+
+        static Color ResolveColor(int value) => value switch
+        {
+            0 => Color.White,
+            1 => Color.Black,
+            _ => throw new InvalidOperationException("Unexpected cell value")
+        };
+    }
+}
--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -1,61 +1,52 @@
 ﻿using GameOfLife;
-using Spectre.Console;
 using Array = GameOfLife.Array;
 
-//ConsoleInteropService.Configure(ConsoleInteropServiceConfiguration.Default);
+ConsoleInteropService.Configure(ConsoleInteropServiceConfiguration.Default);
 
-await Game.Run();
+var patternName = AnsiConsoleExtensions.PromptPattern();
+var speedInMs = 200;
 
-public class Game
+await Game.Play(speedInMs, patternName);
+
+public static class Game
 {
-    static Pattern.Name PromptPattern() =>
-        AnsiConsole.Prompt(
-            new SelectionPrompt<Pattern.Name>()
-                .Title("Select [green]Pattern[/]:")
-                .AddChoices(Enum.GetValues<Pattern.Name>()));
-    public static async Task Run()
+    public static async Task Play(
+        int speedInMs,
+        Pattern.Name patternName)
     {
-        long Generation = 1; // Grid Height and Width
+        var generation = 1;
 
-        var patternName = PromptPattern();
         var pattern = Pattern.Resolve(patternName);
-        Console.WriteLine(pattern.Length);
-        Console.WriteLine(pattern[0].Length);
-        var speed = 300;
 
-        DrawWithCanvas(pattern);
+        AnsiConsoleExtensions.DrawGenerationWithCanvas(pattern);
+        AnsiConsoleExtensions.MarkupLineProperties(
+            speedInMs,
+            patternName,
+            generation);
 
-        AnsiConsole.MarkupLine($"•Speed: [green]{speed}ms[/]");
-        AnsiConsole.MarkupLine($"•Pattern: [green]{patternName}[/]");
-        AnsiConsole.MarkupLine($"•Generation: [green]{Generation}[/]");
-
-        await Task.Delay(300);
+        await Task.Delay(speedInMs);
 
         for (int i = 0; i < 100; i++)
         {
             Console.Clear();
 
-            var nextGrid = ResolveNextGrid(pattern);
+            var nextGrid = ResolveNextGeneration(pattern);
 
-            DrawWithCanvas(nextGrid);
-
-            AnsiConsole.MarkupLine($"•Speed: [green]{speed}ms[/]");
-            AnsiConsole.MarkupLine($"•Pattern: [green]{patternName}[/]");
-            AnsiConsole.MarkupLine($"•Generation: [green]{Generation}[/]");
-            Generation++;
+            AnsiConsoleExtensions.DrawGenerationWithCanvas(pattern);
+            AnsiConsoleExtensions.MarkupLineProperties(
+                speedInMs,
+                patternName,
+                generation++);
 
             pattern = nextGrid;
-            await Task.Delay(300);
+            await Task.Delay(speedInMs);
         }
 
         Console.ReadLine();
     }
 
-    private static int[][] ResolveNextGrid(int[][] xs)
+    private static int[][] ResolveNextGeneration(int[][] xs)
     {
-        const int DEAD = 0;
-        const int ALIVE = 1;
-
         var ys = Array.Create2D(xs.Length, xs[0].Length);
 
         for (int i = 0; i < xs.Length; i++)
@@ -66,32 +57,36 @@ public class Game
 
                 var neighbors = CountNeighbors(xs, i, j);
 
-                if (state is DEAD && neighbors is 0)
-                {
-                    ys[i][j] = DEAD; // Stay dead
-                }
-                else if (state is ALIVE && neighbors <= 1)
-                {
-                    ys[i][j] = DEAD; // Solitude
-                }
-                else if (state is ALIVE && neighbors >= 4)
-                {
-                    ys[i][j] = DEAD; // Overpopulation
-                }
-                else if (state is DEAD && neighbors is 3)
-                {
-                    ys[i][j] = ALIVE; // Reproduction
-                }
-                else
-                {
-                    ys[i][j] = state; // Stay alive
-                }
+                ys[i][j] = ResolveNextState(state, neighbors);
             }
         }
 
         return ys;
+
+        static int ResolveNextState(int currentState, int neighbors)
+        {
+            const int DEAD = 0;
+            const int ALIVE = 1;
+
+            return (currentState, neighbors) switch
+            {
+                (DEAD, 0) => DEAD, // Stay dead
+                (ALIVE, <= 1) => DEAD, // Solitude
+                (ALIVE, >= 4) => DEAD, // Overpopulation
+                (DEAD, 3) => ALIVE, // Reproduction
+
+                _ => currentState
+            };
+        }
     }
 
+    /// <summary>
+    /// Neighbors are the cells surrounding the current cell ([0, 0]):
+    /// [-1][-1], [-1][0], [-1][1],
+    /// [0][-1],  [0][0],  [0][1],
+    /// [1][-1],  [1][0],  [1][1],
+    /// </summary>
+    /// <returns></returns>
     private static int CountNeighbors(int[][] xs, int row, int col)
     {
         int sum = 0;
@@ -112,63 +107,5 @@ public class Game
         }
 
         return sum;
-
-        /*
-        sum += xs[i - 1][j - 1];
-        sum += xs[i - 1][j];
-        sum += xs[i - 1][j + 1];
-
-        sum += xs[i][j - 1];
-        sum += xs[i][j + 1];
-
-        sum += xs[i + 1][j - 1];
-        sum += xs[i + 1][j];
-        sum += xs[i + 1][j + 1];
-        */
-
-        /*
-        [-1][-1], [-1][0], [-1][1],
-        [0][-1], [0][0], [0][1],
-        [1][-1], [1][0], [1][1],
-         */
-    }
-
-    private static void Draw(int[][] xs)
-    {
-        for (int i = 0; i < xs.Length; i++)
-        {
-            for (int j = 0; j < xs[i].Length; j++)
-            {
-                Console.Write(xs[i][j] + " ");
-            }
-
-            Console.WriteLine();
-        }
-    }
-
-    private static void DrawWithCanvas(int[][] xs)
-    {
-        var canvas = new Canvas(xs.Length, xs[0].Length);
-
-        for (int i = 0; i < xs.Length; i++)
-        {
-            for (int j = 0; j < xs[i].Length; j++)
-            {
-                var color = ResolveColor(xs[i][j]);
-
-                // j is the X coordinate (column)
-                // i is the Y coordinate (row)
-                canvas.SetPixel(j, i, color);
-            }
-        }
-
-        AnsiConsole.Write(canvas);
-
-        static Color ResolveColor(int value) => value switch
-        {
-            0 => Color.White,
-            1 => Color.Black,
-            _ => throw new InvalidOperationException("Unexpected cell value")
-        };
     }
 }
